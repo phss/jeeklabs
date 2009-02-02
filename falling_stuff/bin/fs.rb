3a183dad8891d4a90d8ad09dff1781561e6fbd8e
Paulo Schneider <paulo.schneider@gmail.com>
2009-02-02 20:47:53 +0000

    Adding FS again

---
+++ falling_stuff/bin/fs.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby -wKU
+# 
+#  fs.rb
+#  falling_stuff
+#  
+#  Created by Paulo Schneider on 2007-12-25.
+#  Copyright 2007 Jeek Labs. All rights reserved.
+# 
+
+require "lib/falling_stuff"
+
+container = Slick::AppGameContainer.new(GameWindow.new)
+container.set_display_mode(340, 440, false)
+container.set_target_frame_rate(30)
+container.set_minimum_logic_update_interval(30)
+container.set_show_fps(false)
+container.set_verbose(false)
+#container.set_icon("resources/icons/Icone01.ico")
+container.start
