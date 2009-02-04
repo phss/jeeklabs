77b569c3afe57bef9ea855e0e2c742a5bdff7d1f
Paulo Schneider <paulo.schneider@gmail.com>
2009-02-04 21:10:55 +0000

    Created initial rakefile

--- alleway/bin/alleway.rb
+++ alleway/bin/alleway.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby -wKU
 
-LIB_DIR = File.expand_path(File.dirname(__FILE__) + "/../lib/")
-
-require LIB_DIR + "/alleway"
-require LIB_DIR + "/game_engine"
+require "lib/game_engine"
 
 if __FILE__ == $PROGRAM_NAME
   window = GameWindow.new("Alleway", 512, 480)
