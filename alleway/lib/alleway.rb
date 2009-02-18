cf49f0d088bfd30658c74d940b3c132cc297af6b
Paulo Schneider <paulo.schneider@gmail.com>
2009-02-18 21:56:24 +0000

    Moved QuadRepresentation to be under game_engine

--- alleway/lib/alleway.rb
+++ alleway/lib/alleway.rb
@@ -4,7 +4,6 @@ require File.expand_path(File.dirname(__FILE__) + "/game_engine")
 require ALLEWAY_DIR + "/model/pad"
 require ALLEWAY_DIR + "/screens/intro_screen"
 require ALLEWAY_DIR + "/screens/game_screen"
-require ALLEWAY_DIR + "/representations/quad_representation"
 
 # Config params
 WINDOW_WIDTH = 512
