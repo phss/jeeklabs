e0fd281968c39ab2cb284ba94efcd6e1d7810e47
Paulo Schneider <paulo.schneider@gmail.com>
2009-02-24 22:10:25 +0000

    Limit pad to screen

--- alleway/lib/alleway/model/pad.rb
+++ alleway/lib/alleway/model/pad.rb
@@ -6,10 +6,12 @@ class Pad < Deflector
   
   def move_left
     @x -= 5
+    @x = 0 if @x < 0 # FIXME move this logic elsewhere?
   end
   
   def move_right
     @x += 5
+    @x = WINDOW_WIDTH - @width if (@x + @width) > WINDOW_WIDTH # FIXME move this logic elsewhere?
   end
   
   def deflection_modifier(ball)
